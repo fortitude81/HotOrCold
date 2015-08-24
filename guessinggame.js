afc6d77ef2f940e75d1487f846bee33afc490405
Justin <fortitude1981@gmail.com>
2015-08-24 18:46:20 -0500

    Update guessinggame.js

--- guessinggame.js
+++ guessinggame.js
@@ -1,20 +1,19 @@
-var computerChoice = Math.floor(Math.random() * 100) + 1;
-var guess = prompt("chose a number between 1 and 100");
-var totalGuesses = 0;
-
-function guessingGame (){
-totalGuesses++;
-guess = Number(guess);
-
-if (guess <= computerChoice + 10 || guess >= computerChoice + 10){
-	alert ("Getting warm! "+"That's" + totalGuesses + " guesses.");
+	var computerChoice = Math.floor(Math.random() * 100) + 1;
+	var guess = prompt("chose a number between 1 and 100");
+	var totalGuesses = 0;
+	
+	function guessingGame () {
+	  totalGuesses++;
+	  guess = Number(guess);
+	
+	  if (guess <= computerChoice + 10 
+	      || guess >= computerChoice + 10) {
+	      alert ("Getting warm! "+"That's" + totalGuesses + " guesses.");
+	    
+	    } else if (guess === computerChoice) {
+	    alert ("You got it! " + " That was " + 
+	      totalGuesses + " guesses.");     
+	    } else {
+	      alert ("Your cold, keep trying");
+	    }
 	}
-	else if(guess === computerChoice);
-	{
-	alert ("You got it! " + " That was " + totalGuesses + " guesses.");
-	}
-	else
-	{
-	alert ("Your cold, keep trying");
-	}
-}
