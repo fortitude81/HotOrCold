c54dbcf20d3d3d8a9300926e9119f2b0a4b1f29e
Justin <fortitude1981@gmail.com>
2015-09-12 19:54:19 -0500

    Update guessinggame.js

--- guessinggame.js
+++ guessinggame.js
@@ -1,16 +1,34 @@
-var computerChoice = Math.floor(Math.random() * 100) + 1;
-var guess = prompt("chose a number between 1 and 100");
-var totalGuesses = 0;
-		
-totalGuesses++;
-guess = Number(guess);
-	
-if (guess <= computerChoice + 10 || guess >= computerChoice + 10) {
-	alert ("Getting warm! "+"That's" + totalGuesses + " guesses.");
-	    
-} else if (guess === computerChoice) {
-    alert ("You got it! " + " That was " + 
-	      totalGuesses + " guesses.");     
-} else {
-	alert ("Your cold, keep trying");
+var num = Math.floor(Math.random() * 10) + 1;  // generate random #
+var guess;
+var guessCount = 0;
+var correctGuess = false;
+
+guess = prompt('I am thinking of a number between 1 and 10. What is it?');
+guessCount += 1;
+if (parseInt(guess) === num) {
+correctGuess = true;
+
+function hint() {
+var diff;
+var guessInt = parseInt(guess);  //turn guesses into interger
+
+if (guess == num){			//which number is bigger, guess or number
+	alert(“Correct!”);
+} else if (guessInt > number) {
+diff = guessInt – number;
+} else if (guessInt < number) {
+diff = number – guessInt;
 }
+
+if (diff >= 1 && diff <= 10) {
+	alert(“Very Hot”);
+} else if (diff < 10 && diff <= 20){
+	alert(“Hot”);
+} else if (diff < 20 && diff <= 30){
+	alert(“Warm”);
+} else if (diff < 30 && diff <= 50){
+	alert(“Cold”);
+} else if ( diff > 50){
+	alert(“Ice Cold”);
+}	
+	 }
