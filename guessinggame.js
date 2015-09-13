a2e6c5d55e97eaedca0ae8bebb0a5e0b31220a3e
Justin <fortitude1981@gmail.com>
2015-09-13 13:16:20 -0500

    Update guessinggame.js

--- guessinggame.js
+++ guessinggame.js
@@ -2,6 +2,7 @@ var num = Math.floor(Math.random() * 10) + 1;  // generate random #
 var guess;
 var guessCount = 0;
 var correctGuess = false;
+var diff;
 
 guess = prompt('I am thinking of a number between 1 and 10. What is it?');
 guessCount += 1;
@@ -9,7 +10,6 @@ if (parseInt(guess) === num) {
 correctGuess = true;
 
 function hint() {
-var diff;
 var guessInt = parseInt(guess);  //turn guesses into interger
 
 if (guess == num){			//which number is bigger, guess or number
